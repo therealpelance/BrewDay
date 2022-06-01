693b4306f1dd50432a2848340a0203d333fc3327
Rob Pelance <rpelance@gmail.com>
2022-05-31 22:57:59 -0400

    Built navbar for react router

--- src/features/Navbar/Navbar.jsx
+++ src/features/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
+
     return (
-        <div className='nav-container'>
-            
-        </div>
+        <nav className='nav-container'>
+            <NavLink to='/' className='link'>Near Me</NavLink>
+            <NavLink to='/search' className='link'>Search</NavLink>
+            <NavLink to='/cities' className='link'>Popular Cities</NavLink>
+            <NavLink to='/planner' className='link'>Planner</NavLink>
+        </nav>
     )
 };
 
