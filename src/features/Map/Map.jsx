f5283942e44936c8cbba83cf4e1c4905449945e1
Rob Pelance <rpelance@gmail.com>
2022-06-06 17:15:09 -0400

    Added MapBox GL

--- src/features/Map/Map.jsx
+++ src/features/Map/Map.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Map.css';
 
 function Map(props) {
     return (
         <div>
-            
+            {/* <img src='http://beermapping.com/maps/proxymaps.php?locid=972&amp;d=1&amp;d=5' alt='map' /> */}
+            <iframe src='http://beermapping.com/maps/blogproxy.php?locid=1000&amp;d=1&amp;type=norm' className='Map' title='map'></iframe>
         </div>
     );
 }
--- src/pages/NearMe/NearMe.jsx
+++ src/pages/NearMe/NearMe.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import FilterBox from '../../features/FilterBox/FilterBox';
+import LocationList from '../../features/LocationList/LocationList';
+import Map from '../../features/Map/Map';
+import './NearMe.css';
 
 function NearMe () {
     return (
-        <div>
+        <div className='Page-container'>
             <p>Near Me</p>
+            <div className="Content-container">
+                <div className='Map-container'>
+                    <Map />
+                </div>
+                <div className="Locations-container">
+                    <div className="Filters-container">
+                        <FilterBox />
+                    </div>
+                    <div className="LocationsList-container">
+                        <LocationList />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
