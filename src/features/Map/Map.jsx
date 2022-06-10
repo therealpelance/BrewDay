08c2471df1e9afd1ea442babf924ae91af4395be
Rob Pelance <rpelance@gmail.com>
2022-06-10 06:10:36 -0400

    Rendered map component

--- src/features/Map/Map.jsx
+++ src/features/Map/Map.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './Map.css';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHRzZGV2IiwiYSI6ImNsNDdrZ29yYjBpdHAza3FpYXNscGtqNWUifQ.9qYrfbdfIn0EFctT_EY0ew'
+
 function Map(props) {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-85.14);
+    const [lat, setLat] = useState(41.08);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect (() => {
+        if (map.current) return; // Makes sure map only initializes once
+        map.current = new mapboxgl.Map ({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
     return (
         <div>
-            {/* <img src='http://beermapping.com/maps/proxymaps.php?locid=972&amp;d=1&amp;d=5' alt='map' /> */}
-            <iframe src='http://beermapping.com/maps/blogproxy.php?locid=1000&amp;d=1&amp;type=norm' className='Map' title='map'></iframe>
+            <div ref={mapContainer} className="Map-portal"></div>
         </div>
     );
 }
