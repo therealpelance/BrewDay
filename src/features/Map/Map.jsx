e258a4202afff39e604e21e69159fe291e45d53f
Rob Pelance <rpelance@gmail.com>
2022-06-13 03:34:45 -0400

    Implement Redux store for map

--- src/features/Map/Map.jsx
+++ src/features/Map/Map.jsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './Map.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    setLatitude,
+    setLongitude,
+    setZoom
+} from './mapSlice';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZHRzZGV2IiwiYSI6ImNsNDdrZ29yYjBpdHAza3FpYXNscGtqNWUifQ.9qYrfbdfIn0EFctT_EY0ew'
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHRzZGV2IiwiYSI6ImNsNDdrZ29yYjBpdHAza3FpYXNscGtqNWUifQ.9qYrfbdfIn0EFctT_EY0ew';
 
 function Map(props) {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-85.14);
-    const [lat, setLat] = useState(41.08);
-    const [zoom, setZoom] = useState(9);
+    const mapProps = useSelector((state) => state.map);
+    const {lat, lng, zoom} = mapProps;
+    const dispatch = useDispatch();
 
     useEffect (() => {
         if (map.current) return; // Makes sure map only initializes once
@@ -20,6 +26,15 @@ function Map(props) {
             center: [lng, lat],
             zoom: zoom
         });
+    });
+
+    useEffect (() => {
+        if (!map.current) return; // Waits for map to initialize
+        map.current.on('move', () => {
+            dispatch(setLatitude(map.current.getCenter().lat.toFixed(4)));
+            dispatch(setLongitude(map.current.getCenter().lng.toFixed(4)));
+            dispatch(setZoom(map.current.getZoom().toFixed(2)));
+        })
     });
 
     return (
---
+++ src/features/Map/mapSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoading: false,
+    locations: {},
+    lat: 41.08,
+    lng: -85.14,
+    zoom: 9
+};
+
+export const mapSlice = createSlice({
+    name: 'map',
+    initialState,
+    reducers: {
+        setLatitude(state, action) {
+            state.lat = action.payload;
+        },
+        setLongitude(state, action) {
+            state.lng = action.payload;
+        },
+        setZoom(state, action) {
+            state.zoom = action.payload;
+        }
+    }
+});
+
+export const {
+    setLatitude,
+    setLongitude,
+    setZoom
+} = mapSlice.actions;
+
+export default mapSlice.reducer;
